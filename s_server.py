ae942593b85c32ea93333c8f857d01369db82095
tzietkowski <tzietkowski@gmail.com>
2021-06-11 10:38:30 +0200

    add json format

--- s_server.py
+++ s_server.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
 import socket
 import time
+import json
 
 HOST = '127.0.0.1'
 PORT = 65432
 TIME_START_SERVER = time.time()
 
 def uptime_command():
-    return time.strftime("%H:%M:%S", time.gmtime(time.time() - TIME_START_SERVER))
+    return json.dumps(time.strftime("%H:%M:%S", time.gmtime(time.time() - TIME_START_SERVER)))
 
 def info_command():
-    return str('Ver 1.0, Server start: ' + time.ctime(TIME_START_SERVER))
+    return json.dumps('Ver 1.0, Server start: ' + time.ctime(TIME_START_SERVER))
 
 def help_command():
-    return str("""
-            uptime - czas zycia serwera,\n
-            info - numer wersji serwera, data utworzenia,\n
-            help - pomoc,\n
-            stop - zamyka serwer i klienta\n
-            """)
-
-def stop_command():
-    pass
+    return json.dumps({
+            'uptime' : 'czas zycia serwera',
+            'info' : 'numer wersji serwera, data utworzenia',
+            'help' : 'pomoc',
+            'stop' : 'zamyka serwer i klienta'
+    })
 
 def wrong_command():
-    return str('Wrong command')
+    return json.dumps('Invalid command')
 
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     server.bind((HOST,PORT))
     server.listen()
     print("Wait to connect client")
