6fe7886a9fdb1b0470ca323663a0b189a40a7c17
tzietkowski <tzietkowski@gmail.com>
2021-06-16 12:44:08 +0200

    adding function check_name and update function add_user

--- users.py
+++ users.py
@@ -1,36 +1,55 @@
 import json
 import os
 
-DATA_FILE_NAME = 'new1.json'
+DATA_FILE_NAME = 'data_user.json'
 
-def load_data(fname:str) -> dict:
+def load_data(fname = DATA_FILE_NAME) -> list:
     if os.path.isfile(fname):
         # File exists
         with open(fname, 'r') as outfile:
             data = json.load(outfile)
-    else: 
+            #add checking data
+    else:
         # Create file with admin user
         with open(fname, 'w') as outfile:
-            data = add_user('admin', hash('admin'), 1)
+            data = {'data': [{
+                'name': 'admin',
+                'password': 3815113479867748683,
+                'group': 'admin',
+                'messages': {'limit': 0, 'char': 256, 'text': ['']}}]}
             json.dump(data, outfile)
-    return data
+    return data.get('data')
 
-def add_user(name:str, password:str, group:str) -> dict:
-    if os.path.isfile(DATA_FILE_NAME):
-        tmp = load_data(DATA_FILE_NAME)
-    empty_user = {
-    'user_' + name :{
-        'name' : name,   #user name
-        'password' : password,  #hash
-        'group' : group,   #user/admin
-        'messages': {       #message
-            'limit' : 5,    #max message in box
-            'char' : 256,   #max character in message
-            'text' : ['']   #list of message
-        }
-    }
-    
-}
-    return empty_user
 
-add_user('tom', 'dupa', 1)
+def save_data(data:list, database = DATA_FILE_NAME):
+    with open(database, 'w') as outfile:
+        json.dump({'data': data}, outfile)
+
+def check_name(name:str, data_base:list) -> bool:
+    for user in data_base:
+        if user.get('name') == name:
+            return False
+    return True
+
+
+def add_user(name:str, password = '', group = 'user') -> bool:
+    temp_base = load_data()
+    if check_name(name,temp_base):
+        new_user = {
+            'name' : name,   #user name
+            'password' : hash(password),  #hash
+            'group' : 'admin' if group == 'admin' else 'user',   #user/admin
+            'messages': {       #message
+                'limit' : 0 if group == 'admin' else 5,    #max message in box
+                'char' : 256,   #max character in message
+                'text' : ['']   #list of message
+            }}
+        temp_base.append(new_user)
+        save_data(temp_base)
+        return True
+    print('uzytkownik juz jest')
+    #wyslij komunikat ze podana nazwa jest zajeta
+    return False
+
+def del_user(name:str):
+    pass
