b4ea4cb5856ace686764622a771ea0dc51c22ddf
tzietkowski <tzietkowski@gmail.com>
2021-06-18 16:07:07 +0200

    adding user Class

--- users.py
+++ users.py
@@ -1,60 +1,73 @@
 import json
 import os
 
-DATA_FILE_NAME = 'data_user.json'
+class Data_base():
+    
+    database_file = 'data_user_class.json'
 
-def load_data(fname = DATA_FILE_NAME) -> list:
-    if os.path.isfile(fname):
-        with open(fname, 'r') as outfile:
-            data = json.load(outfile)
-    else:
-        with open(fname, 'w') as outfile:
-            data = {'data': [{
-                'name': 'admin',
-                'password': 3815113479867748683,
-                'group': 'admin',
-                'messages': {'limit': 0, 'char': 256, 'text': ['']}}]}
-            json.dump(data, outfile)
-    return data.get('data')
+    def __init__(self) -> None:
+        self.database = self.load_data()
+        self.users = User('tom','tom','tom',['a'])
 
-def save_data(data:list, database = DATA_FILE_NAME):
-    with open(database, 'w') as outfile:
-        json.dump({'data': data}, outfile)
+    def load_data(self):
+        if os.path.isfile(Data_base.database_file):
+            with open(Data_base.database_file, 'r') as outfile:
+                data = json.load(outfile)
+        else:
+            with open(Data_base.database_file, 'w') as outfile:
+                data = {'data': [{
+                    'name': 'admin',
+                    'password': 3815113479867748683,
+                    'group': 'admin',
+                    'messages': {'limit': 0, 'char': 256, 'text': ['']}}]}
+                json.dump(data, outfile)
 
-def check_name(name:str, data_base:list) -> bool:
-    for user in data_base:
-        if user.get('name') == name:
+        
+        return data.get('data')
+     
+    def save_data(data):
+        with open(Data_users.database_file, 'w') as outfile:
+            json.dump({'data': data}, outfile)
+        
+    def add_user(self):
+        pass
+
+    def del_user(self):
+        pass
+
+
+class User:
+    
+    def __init__(self, login:str, password:str, message:list, group = 'user') -> None:
+        self.login = login
+        self.__password = password
+        self.__group = group
+        self.__message = message
+        self.__max_message = 5 if group == 'user' else 0
+        self.__count_message = len(message)
+        self.__max_char = 255
+
+    def check_password(self,input_password:str) -> bool:
+        return self.__password == input_password
+
+    def add_message(self,from_user:str, new_message:str)->bool:
+        if (self.__group == 'user' and len(self.__message) == self.__max_message) or (len(new_message) > self.__max_char):
             return False
-    return True
-
-def add_user(name:str, password = '', group = 'user') -> bool:
-    temp_base = load_data()
-    if check_name(name,temp_base):
-        new_user = {
-            'name' : name,   #user name
-            'password' : hash(password),  #hash
-            'group' : 'admin' if group == 'admin' else 'user',   #user/admin
-            'messages': {       #message
-                'limit' : 0 if group == 'admin' else 5,    #max message in box
-                'char' : 256,   #max character in message
-                'text' : ['']   #list of message
-            }}
-        temp_base.append(new_user)
-        save_data(temp_base)
+        self.__count_message += 1
+        self.__message.append((self.__count_message, from_user, new_message))
         return True
-    #wyslij komunikat ze podana nazwa jest zajeta
-    return False
 
-def del_user(name:str) -> bool:
-    temp_base = load_data()
-    #no user in database
-    if check_name(name,temp_base):
+    def del_message(self, id)-> bool:
+        if 0 < id < self.__count_message + 1:
+            del self.__message[id - 1]
+            return True
         return False
-    for index, user in enumerate(temp_base):
-        if user.get('name') == name:
-            del temp_base[index]
-            save_data(temp_base)
-        return True
 
+    def list_message(self)->list:
+        return self.__message
 
+    def change_password(self,new_passsword:str):
+        self.__password = new_passsword
 
+DB = Data_base()
+print(DB.users.login)
