c501a5bb22591af82cf7809b735fa7475771a125
tzietkowski <tzietkowski@gmail.com>
2021-06-22 16:57:27 +0200

    Creating a user class

--- s_client.py
+++ s_client.py
@@ -7,14 +7,22 @@ class Client:
     def __init__(self, host ='127.0.0.1', port = 65432) -> None:
         self.login = False
         self.connected_with_serwer = False
-        self.connect(host, port)
-        self.command()
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.connect(host, port)
+            self.command()
+        except:
+            print('Server not found')
+        finally:
+            self.disconnect()
 
     def connect(self, host:str, port:int)-> None:
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect((host,port))
         self.connected_with_serwer = True
         print('Connected to the server')
+
+    def disconnect(self)->None:
+        self.client.close()
 
     def command(self)-> None:
         while self.connected_with_serwer:
@@ -26,8 +34,9 @@ class Client:
 
     def login_user(self)->bool:
         while True:
-            self.send_command(json.dumps({'user' : input('Login: '), 'password': input('Password: ')}))
-            #self.client.send(json.dumps({'user' : input('Login: '), 'password': input('Password: ')}).encode())
+            self.send_command(json.dumps({\
+                    'user' : input('Login: '), \
+                    'password': input('Password: ')}))
             if json.loads(self.client.recv(2048))['answer'] == 'ok':
                 self.login = True
                 print('Logged in')
@@ -35,11 +44,11 @@ class Client:
             print('Wrong login or password')
 
     def send_command(self, command:str):
-        print('komenda wyslana: ' + command)
+        print('Client: ' + command)
         self.client.send(command.encode())
-        
+
     def recv_command(self):
-        print('komenda odebrana')
-        print(self.client.recv(2048).decode())
+        print('Server: ' + self.client.recv(2048).decode())
+
 
 bobek = Client()
