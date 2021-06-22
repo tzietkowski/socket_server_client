b324000df024eca1a251305a0409d702d7c16d61
tzietkowski <tzietkowski@gmail.com>
2021-06-22 16:40:08 +0200

    Creating a user class

--- s_client.py
+++ s_client.py
@@ -1,21 +1,45 @@
 #!/usr/bin/python3
 import socket
+import json
 
-HOST = '127.0.0.1'
-PORT = 65432
+class Client:
 
-def main():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((HOST,PORT))
-    message = client.recv(64)
-    print(message.decode())
-    while True:
-        answer = input("Command: ")
-        client.send(answer.encode())
-        if answer == 'stop':
-            client.close()
-            return
-        message = client.recv(2048)
-        print(message.decode())
+    def __init__(self, host ='127.0.0.1', port = 65432) -> None:
+        self.login = False
+        self.connected_with_serwer = False
+        self.connect(host, port)
+        self.command()
 
-main()
+    def connect(self, host:str, port:int)-> None:
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((host,port))
+        self.connected_with_serwer = True
+        print('Connected to the server')
+
+    def command(self)-> None:
+        while self.connected_with_serwer:
+            print('Please log in')
+            self.login_user()
+            while self.login:
+                self.send_command(input('command: '))
+                self.recv_command()
+
+    def login_user(self)->bool:
+        while True:
+            self.send_command(json.dumps({'user' : input('Login: '), 'password': input('Password: ')}))
+            #self.client.send(json.dumps({'user' : input('Login: '), 'password': input('Password: ')}).encode())
+            if json.loads(self.client.recv(2048))['answer'] == 'ok':
+                self.login = True
+                print('Logged in')
+                return True
+            print('Wrong login or password')
+
+    def send_command(self, command:str):
+        print('komenda wyslana: ' + command)
+        self.client.send(command.encode())
+        
+    def recv_command(self):
+        print('komenda odebrana')
+        print(self.client.recv(2048).decode())
+
+bobek = Client()
