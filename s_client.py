07dc5eb95dff1ebfa8f8a60dc42cffead88ff016
tzietkowski <tzietkowski@gmail.com>
2021-06-22 17:19:05 +0200

    Add docstring and except socket.error

--- s_client.py
+++ s_client.py
@@ -1,8 +1,10 @@
+"""Client"""
 #!/usr/bin/python3
 import socket
 import json
 
 class Client:
+    """Class client"""
 
     def __init__(self, host ='127.0.0.1', port = 65432) -> None:
         self.login = False
@@ -11,20 +13,29 @@ class Client:
         try:
             self.connect(host, port)
             self.command()
-        except:
-            print('Server not found')
+        except socket.error:
+            print('Server error')
         finally:
             self.disconnect()
 
+
     def connect(self, host:str, port:int)-> None:
+        """Function connect to serwer"""
+
         self.client.connect((host,port))
         self.connected_with_serwer = True
         print('Connected to the server')
 
+
     def disconnect(self)->None:
+        """Function disconnect to serwer"""
+
         self.client.close()
 
+
     def command(self)-> None:
+        """Function of sending and receiving commands"""
+
         while self.connected_with_serwer:
             print('Please log in')
             self.login_user()
@@ -32,7 +43,10 @@ class Client:
                 self.send_command(input('command: '))
                 self.recv_command()
 
+
     def login_user(self)->bool:
+        """Function login user in server"""
+
         while True:
             self.send_command(json.dumps({\
                     'user' : input('Login: '), \
@@ -43,11 +57,16 @@ class Client:
                 return True
             print('Wrong login or password')
 
+
     def send_command(self, command:str):
-        print('Client: ' + command)
+        """Function send command to server"""
+
         self.client.send(command.encode())
 
+
     def recv_command(self):
+        """Function receiving from server"""
+
         print('Server: ' + self.client.recv(2048).decode())
 
 
