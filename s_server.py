ab0eabe8ddd0bb593f1dc72358cb8a9c955a1985
tzietkowski <tzietkowski@gmail.com>
2021-06-25 13:44:25 +0200

    add function checking premmison

--- s_server.py
+++ s_server.py
@@ -1,3 +1,4 @@
+"""Server"""
 #!/usr/bin/python3
 import socket
 import time
@@ -7,7 +8,8 @@ from users import DataBase
 
 class Server:
     """Class server"""
-
+    user_admin1 = False
+    
     def __init__(self, host = '127.0.0.1', port = 65432) -> None:
         self.time_start = time.time()
         self.db = DataBase()
@@ -15,13 +17,15 @@ class Server:
         self.db.add_user('tom','1','user')
         self.server_run = False
         self.login = False
+        self.user_admin = False
         self.server_scoket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.commands = {'info': {'func': self.info ,'help': 'Info about the server version'},\
                         'stop': {'func': self.stop_server ,'help':'Server stop'},\
                         'help': {'func': self.help ,'help':'Server stop'},\
+                        'new_user': {'func': self.new_user ,'help':'Add new user: name_user:password'},\
                         'uptime': {'func': self.uptime ,'help':'Server lifetime'}\
                             }
-        
+
         try:
             self.server = self.start_server(host, port)
             self.login = self.login_user()
@@ -32,10 +36,21 @@ class Server:
             print('The end')
             self.stop_server()
 
+    def admin_required(func):
+        def wrapper(self, *args, **kwargs):
+            if self.user_admin:
+                print('admin')
+                return func(self, *args, **kwargs)
+            else:
+                print('nieadmin')
+                return 'No premissed'
+
+        return wrapper
+
 
     def start_server(self, host:str, port:int) -> socket:
         """Function start server"""
-        
+
         self.server_scoket.bind((host, port))
         self.server_scoket.listen()
         print("Wait to connect client")
@@ -54,16 +69,19 @@ class Server:
     def login_user(self)-> bool:
         """Function login user"""
 
-        while self.login == False:
+        while not self.login:
             message = json.loads(self.server.recv(2024).decode())
-            if message['command']['user'] in self.db.users.keys() and self.db.users[message['command']['user']].check_password(message['command']['password']):
-                self.server.send(json.dumps({'user': message['command']['user'], 'answer': 'ok'}).encode())
+            if message['command']['user'] in self.db.users.keys() and self.db.users[\
+                message['command']['user']].check_password(message['command']['password']):
+                self.server.send(json.dumps(\
+                    {'user': message['command']['user'], 'answer': 'ok'}).encode())
                 print('login user: ' + message['command']['user'])
+                self.user_admin = self.db.users[message['command']['user']].check_admin()
                 return True
             else:
-                self.server.send(json.dumps({'user': message['user'], 'answer': 'wrong login or password'}).encode())
-
-
+                self.server.send(json.dumps({\
+                    'user': message['user'], 'answer': 'wrong login or password'}).encode())
+        return False
 
     def command(self)-> None:
         """Function run command"""
@@ -76,7 +94,7 @@ class Server:
                 self.send(message['user'], self.commands[message['command']]['func']())
             else:
                 self.send(message['user'], 'Wrong command')
-                
+
 
     def send(self, user:str, message:str)-> None:
         """Function send message to client"""
@@ -84,11 +102,11 @@ class Server:
         self.server.send(json.dumps({'user': user, 'command': message}).encode())
 
 
-
     def info(self)-> str:
         """Function info about the server version"""
 
-        return ('Ver 2.0, Server start: ' + time.ctime(self.time_start))
+        return 'Ver 2.0, Server start: ' + time.ctime(self.time_start)
+
 
     def help(self)-> str:
         """Function help - list of commands"""
@@ -96,9 +114,15 @@ class Server:
         lista = 'Tu bedzie lista '
         return lista
 
+
     def uptime(self) -> None:
+        """Function server lifetime"""
 
         return time.strftime("%H:%M:%S", time.gmtime(time.time() - self.time_start))
 
+    @admin_required
+    def new_user(self) -> str:
+        return 'udalo sie'
+        
 
 bobek = Server()
